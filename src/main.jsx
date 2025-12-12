92d625be9046868abf693f4b2ed441874acbeaa3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-12 01:43:31 +0000

    main.jsx: Imported Homepage, Shop, Cart, createBrowserRouter, RouterProvider. Set up router with children property. App can link to each page

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import HomePage from './components/Homepage/homePage.jsx'
+import Shop from './components/ShopPage/ShopPage.jsx'
+import Cart from './components/cartPage/cartPage.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {path: "/", element: <HomePage />},
+      {path: "shop", element: <Shop />},
+      {path: "cart", element: <Cart />},
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
