8d36614fe9e87432bd98e6c3b6b23590f4019b5c
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-17 11:57:46 +0000

    NavBar.jsx: Added noOfItems prop

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router";
 import styles from './Navbar.module.css'
 
-function Navbar() {
+function Navbar({noOfItems}) {
     return (
         <nav>
             <Link className="link" to="/">Costcutters</Link>
             <Link className="link" to="/">Home</Link>
             <Link className="link" to="/shop">Shop</Link>
-            <Link className="link" to="/cart">Cart</Link>
+            <Link className="link" to="/cart">Cart ({noOfItems})</Link>
         </nav>
     )
 };
