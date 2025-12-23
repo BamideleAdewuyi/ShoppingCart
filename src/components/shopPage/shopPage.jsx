80807f2a8d523777e42cbb1876e061265b4507a7
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-23 17:44:17 +0000

    ShopPage.jsx: Set up useOutletContext

--- src/components/shopPage/shopPage.jsx
+++ src/components/shopPage/shopPage.jsx
@@ -1,9 +1,12 @@
 import { Link, useOutletContext } from "react-router";
 
 function Shop() {
+    const [noOfItems, setNoOfItems, items, setItems] = useOutletContext();
+
     return (
         <div>
             <h1>ITEMS</h1>
+            
         </div>
     )
 }
