3867108f22732834083f996f9bfb07c420ff3de4
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-24 22:37:52 +0000

    ShopPage.jsx: moved getItems async function inside useEffect to solve linting error

--- src/components/shopPage/shopPage.jsx
+++ src/components/shopPage/shopPage.jsx
@@ -5,20 +5,21 @@ function Shop() {
     const [noOfItems, setNoOfItems, basket, setBasket] = useOutletContext();
     const [items, setItems] = useState({});
 
-    async function getItems() {
-        try {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const json = await res.json();
-            setItems(json);
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
+    
     useEffect(() => {
+        async function getItems() {
+            try {
+                const res = await fetch('https://fakestoreapi.com/products');
+                const json = await res.json();
+                setItems(json);
+            } catch (error) {
+                console.log(error);
+            }
+        }
         getItems();
     }, [])
     
+    console.log(items[4])
     return (
         <div>
             <h1>ITEMS</h1>
