e8caec226675e7f7f2d8f6648e407ae4a0cb0f21
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-24 16:38:31 +0000

    ShopPage.jsx: Changed useContext variables from items and setItems to basket and setBasket. Imported useState and set up state variable items. Wrote async function getItems in order to get all items from fakestore api

--- src/components/shopPage/shopPage.jsx
+++ src/components/shopPage/shopPage.jsx
@@ -1,8 +1,24 @@
 import { Link, useOutletContext } from "react-router";
+import { useEffect, useState } from "react";
 
 function Shop() {
-    const [noOfItems, setNoOfItems, items, setItems] = useOutletContext();
+    const [noOfItems, setNoOfItems, basket, setBasket] = useOutletContext();
+    const [items, setItems] = useState({});
 
+    async function getItems() {
+        try {
+            const res = await fetch('https://fakestoreapi.com/products');
+            const json = await res.json();
+            setItems(json);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getItems();
+    }, [])
+    
     return (
         <div>
             <h1>ITEMS</h1>
