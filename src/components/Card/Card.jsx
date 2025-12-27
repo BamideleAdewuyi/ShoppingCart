fdf39bea86ab78221231d5ead4c5cc6ab2d0e5e8
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-27 14:19:11 +0000

    Card.jsx: Wrote increment and decrement functions. Called on plus and minus buttons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react';
 
 function Card({title, price, image}) {
     const [quantity, setQuantity] = useState(0)
+
+    function increment() {
+        setQuantity(quantity + 1)
+    };
+
+    function decrement() {
+        if (quantity != 0) setQuantity(quantity - 1);
+    };
+
     return (
         <div className={styles.card}>
             <form action="">
@@ -10,9 +19,9 @@ function Card({title, price, image}) {
                 <h2 className={styles.itemName}>{title}</h2>
                 <h3 className={styles.price}>£{price}</h3>
                 <div className={styles.quantity}>
-                    <button type="button">-</button>
+                    <button type="button" onClick={decrement}>-</button>
                     <input type="number" value={quantity}/>
-                    <button type="button">+</button>
+                    <button type="button" onClick={increment}>+</button>
                 </div>
                 <button>Add to Cart</button>
             </form>
