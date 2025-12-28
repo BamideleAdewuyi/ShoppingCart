160ac1c233fa38c2b9a4bfd7bc4c5633e8a02643
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-28 23:58:31 +0000

    CartPage.jsx: Imported Card module and set up div for cards

--- src/components/cartPage/cartPage.jsx
+++ src/components/cartPage/cartPage.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router";
-import styles from "./CartPage.module.css"
+import styles from "./CartPage.module.css";
+import Card from "../Card/Card";
 
 function Cart() {
     return (
         <div>
             <h1>Cart</h1>
-            
+            <div className={styles.cards}>
+                
+            </div>
         </div>
     )
 }
