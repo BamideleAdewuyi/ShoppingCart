65409bcce385299e5819cb7d40334b8e808610dc
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-29 22:11:17 +0000

    CartPage.jsx: Rendered cart as ShopCard elements

--- src/components/cartPage/cartPage.jsx
+++ src/components/cartPage/cartPage.jsx
@@ -1,13 +1,19 @@
 import { Link, useOutletContext } from "react-router";
 import styles from "./CartPage.module.css";
+import ShopCard from "../ShopCard/ShopCard";
 
 function Cart() {
     const [noOfItems, setNoOfItems, cart, setCart] = useOutletContext();
+    function updateCart() {
+
+    };
     return (
         <div>
             <h1>Cart</h1>
             <div className={styles.cards}>
-
+                {Object.keys(cart).map(key => (
+                    <ShopCard key={[key].id} title={key} price={cart[key].price} image={cart[key].image}  updateCart={updateCart} amount={cart[key].quantity}/>
+                ))}
             </div>
         </div>
     )
