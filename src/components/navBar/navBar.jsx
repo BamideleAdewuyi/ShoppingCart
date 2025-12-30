ae7ad56844464bc63b86bbb98679832931b1ef23
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-30 02:22:25 +0000

    Navbar.jsx: Set up useState variable noOfItems along with useEffect that calculates total items every time cart is updated. Passed into Cart Link in Navbar

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router";
 import styles from './Navbar.module.css'
+import { useEffect, useState } from "react";
 
 function Navbar({cart}) {
+    const [noOfItems, setNoOfItems] = useState(0);
+
+    useEffect(() => {
+        async function noOfItems(cart) {
+            try{
+                const total = Object.keys(cart).reduce(function (previous, key) {
+            return previous + cart[key].quantity
+            }, 0)
+            setNoOfItems(total)
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        noOfItems(cart);
+    }, [cart])
+    
     return (
         <nav>
             <Link className="link" to="/">Costcutters</Link>
             <Link className="link" to="/">Home</Link>
             <Link className="link" to="/shop">Shop</Link>
-            <Link className="link" to="/cart">Cart ()</Link>
+            <Link className="link" to="/cart">Cart ({noOfItems})</Link>
         </nav>
     )
 };
