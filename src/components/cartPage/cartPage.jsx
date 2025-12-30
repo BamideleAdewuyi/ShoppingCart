d6ddae6f1b26aa4659ce0d3809d6c2e30f689916
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-30 14:06:41 +0000

    CartPage.jsx: Wrote updateCart function and changed return so items with quantity less than 1 are not rendered. Can now edit cart from CartPage

--- src/components/cartPage/cartPage.jsx
+++ src/components/cartPage/cartPage.jsx
@@ -3,17 +3,27 @@ import styles from "./CartPage.module.css";
 import ShopCard from "../ShopCard/ShopCard";
 
 function Cart() {
-    const [noOfItems, setNoOfItems, cart, setCart] = useOutletContext();
-    
-    function updateCart() {
+    const [cart, setCart] = useOutletContext();
 
+    function updateCart(title, quantity, price, image, id) {
+        setCart({
+                ...cart,
+                [title]: {
+                    quantity: quantity,
+                    image: image,
+                    price: price,
+                    id: id
+                }                
+            })
     };
 
     return (
         <div>
             <h1>Cart</h1>
             <div className={styles.cards}>
-                {Object.keys(cart).map(key => (
+                {Object.keys(cart)
+                .filter(key => cart[key].quantity > 0)
+                .map(key => (
                     <ShopCard key={cart[key].id} title={key} price={cart[key].price} image={cart[key].image} updateCart={updateCart} amount={cart[key].quantity} id={cart[key].id} buttonText="Update"/>
                 ))}
             </div>
