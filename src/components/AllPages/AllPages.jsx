d27c0e49c7923de324127f056988964aa5cb653d
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-12-30 02:04:23 +0000

    AllPages.jsx: Removed noOfItems state variable. Navbar just gets cart passed to it

--- src/components/AllPages/AllPages.jsx
+++ src/components/AllPages/AllPages.jsx
@@ -3,12 +3,11 @@ import { Outlet } from "react-router";
 import { useState } from "react";
 
 function AllPages() {
-    const [noOfItems, setNoOfItems] = useState(0);
     const [cart, setCart] = useState({})
     return (
         <div>
-            <Navbar noOfItems={noOfItems}/>
-            <Outlet context={[noOfItems, setNoOfItems, cart, setCart]}/>
+            <Navbar cart={cart}/>
+            <Outlet context={[cart, setCart]}/>
         </div>
     )
 }
